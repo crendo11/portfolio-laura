0eda9ed06bdf53451275a6e6a5259f3b1204e71c
crendo11 <crendoc11@gmail.com>
2025-07-25 22:42:41 +0200

    using local env and changed title of the page

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
-  projectId: 'jw0ycs1a', // <-- Replace with your project ID
-  dataset: 'production',        // <-- Replace with your dataset name
-  apiVersion: '2025-07-25',     // Use today's date or the date you set up Sanity
-  useCdn: true,                 // `false` if you want fresh data
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  useCdn: true,
 })
