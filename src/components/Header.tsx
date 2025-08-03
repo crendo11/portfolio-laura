e253a47c7b2b5150d0e3d606673ba8a6d67d50eb
crendo11 <crendoc11@gmail.com>
2025-08-03 16:26:06 +0200

    full width header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
 
   return (
     <header>
-      <div className={`fixed bg-transparent flex items-center justify-between z-50 transition-all ease-linear duration-200`}
+      <div className={`fixed w-full bg-transparent flex items-center justify-between z-50 transition-all ease-linear duration-200`}
            style={{ color: 'black', fontSize: `${isScroll ? "1rem": "2rem"}` }}>
         <span className="font-bold text-lg ml-[10px] mr-[10px]">Leidy Laura Rendon</span>
         <span className="font-normal text-base ml-[10px] mr-[10px]">about</span>
