936174d82cea347cecf010ea89e23ee43b6c1706
crendo11 <crendoc11@gmail.com>
2025-08-08 19:30:05 +0200

    testing new way to revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { revalidateTag } from 'next/cache'
+import { revalidateTag, revalidatePath } from 'next/cache'
 
 export async function POST(req: NextRequest) {
   const secret = process.env.REVALIDATE_SECRET
@@ -10,6 +10,7 @@ export async function POST(req: NextRequest) {
   }
 
   // Revalidate all pages that use the 'projects' tag
-  revalidateTag('projects')
+  await revalidateTag('projects')
+  await revalidatePath('/') // Add this line to revalidate the homepage
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
